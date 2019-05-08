86065e88c18c0d14a59b46cb1597c14578ca9c06
Vladislav <drimacus182@gmail.com>
2019-05-08 18:11:27 +0300

    draggable earth and moon

--- main.js
+++ main.js
@@ -8,9 +8,37 @@
 		var height = box.height;
 
 
-		console.log(box)
+		var earth = d3.select("#earth")
+			.datum({x: width/2, y: height/2})
+			.attr("cx", d => d.x)
+			.attr("cy", d => d.y)
+			.call(d3.drag()
+	        .on("start", dragstarted)
+	        .on("drag", dragged)
+	        .on("end", dragended))
+
+		var moon = d3.select("#moon")
+			.datum({x: width/4, y: height/2})
+			.attr("cx", d => d.x)
+			.attr("cy", d => d.y)
+			.call(d3.drag()
+	        .on("start", dragstarted)
+	        .on("drag", dragged)
+	        .on("end", dragended))
 
 
+
+		function dragstarted(d) {
+		  	d3.select(this).raise().classed("active", true);
+		}
+
+		function dragged(d) {
+		  d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
+		}
+
+		function dragended(d) {
+		  d3.select(this).classed("active", false);
+		}
 
 	} 
 })()
