425bad465ee6534bcce8c26eccd93b7ddfab9069
Vladislav <drimacus182@gmail.com>
2019-05-08 18:27:17 +0300

    Фундаментальні сталі

--- main.js
+++ main.js
@@ -27,17 +27,23 @@
 	        .on("end", dragended))
 
 
+		 var G = 6.67384E-11;
+		 var M = 5.972E24;
+		 var d = 384000;
+		 var v = 3683 * 1000 / 3600; // м/c
+
+
 
 		function dragstarted(d) {
 		  	d3.select(this).raise().classed("active", true);
 		}
 
 		function dragged(d) {
-		  d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
+		  	d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
 		}
 
 		function dragended(d) {
-		  d3.select(this).classed("active", false);
+		  	d3.select(this).classed("active", false);
 		}
 
 	} 
