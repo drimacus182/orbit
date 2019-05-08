3fb1f15594253c2257186f1a4ce0c8d555557f47
vlad <drimacus182@gmail.com>
2019-05-09 02:25:10 +0300

    finally working mechanics

--- main.js
+++ main.js
@@ -7,6 +7,12 @@
 		var width = box.width;
 		var height = box.height;
 
+		var ctx = d3.select("canvas")
+			.attr("width", width)
+			.attr("height", height)
+			.node()
+			.getContext("2d");
+
 
 		var earth = d3.select("#earth")
 			.datum({x: width/2, y: height/2})
@@ -32,7 +38,7 @@
 		var d = 384000000;
 		var v = 3683 * 1000 / 3600; // м/c
 
-		var step = 3600;
+		var dt = 3600;
 
 
 		var real_to_virtual = d3.scaleLinear()
@@ -45,27 +51,43 @@
 		moon_d.vy = v;
 		moon_d.vx = 0;
 
-		var a = 0;
+		var a;
 		var r;
 
 		function tick() {
-			a = G*M/(d^2);
+			a = G*M/(sq(d));
 
-			r = minus(earth_d, moon_d); 
+			r_px = minus(earth_d, moon_d); 
+			r = {x: real_to_virtual.invert(r_px.x), y: real_to_virtual.invert(r_px.y)};
+
 
 			r_length = get_distance({x: 0, y: 0}, r);
 			r_one = {x: r.x/r_length, y: r.y/r_length};
 
 			a_v = scale(r_one, a);
 
-			vx = moon_d.vx + a_v.x * step;
-			vy = moon_d.vy + a_v.y * step; 
 
-			console.log(vx, vy)
+			moon_d.dx_r = moon_d.vx * dt + a_v.x * sq(dt) / 2;
+			moon_d.dy_r = moon_d.vy * dt + a_v.y * sq(dt) / 2; 
+
+			moon_d.x = moon_d.x + real_to_virtual(moon_d.dx_r);
+			moon_d.y = moon_d.y + real_to_virtual(moon_d.dy_r);
+
+			moon
+				.attr("cx", d => d.x)
+				.attr("cy", d => d.y)
+
+			ctx.fillRect(moon_d.x,moon_d.y,2,2);
+
+			console.log(moon_d)
+
+			moon_d.vx = moon_d.vx + a_v.x * dt;
+			moon_d.vy = moon_d.vy + a_v.y * dt; 
+
+
 		}
 
-
-		setInterval(tick, 100);
+		setInterval(tick, 10);
 
 		function dragstarted(d) {
 		  	d3.select(this).raise().classed("active", true);
